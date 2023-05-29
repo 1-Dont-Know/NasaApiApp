0545e221fa5e59329bc1f27491a0cff99bf6ec5c
1-Dont-Know <darshwakdhami@yahoo.com>
2023-05-29 11:29:50 -0700

    Change pagination to show 1 api page per page for the search result and improved the code to fix bugs.

--- app/src/main/java/com/example/nasaapiapp/MainActivity.kt
+++ app/src/main/java/com/example/nasaapiapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.nasaapiapp
 
+import java.lang.Exception
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -18,7 +19,6 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import okhttp3.*
 import java.io.IOException
-import java.lang.Exception
 import kotlin.math.min
 
 // Define data classes for JSON response from NASA API
@@ -37,12 +37,16 @@ data class Item(val data: List<Data>, val links: List<Link>? = null)
 
 // Data represents a "data" object within an item. It contains information about the item such as its title, description, date created, and NASA ID.
 @Serializable
-data class Data(val title: String, val description: String, val date_created: String, val nasa_id: String)
+data class Data(
+    val title: String,
+    val description: String? = null,
+    val date_created: String,
+    val nasa_id: String
+)
 
 // Link represents a "link" object within an item. It contains a URL to a resource associated with the item, such as an image or video.
 @Serializable
 data class Link(val href: String)
-
 
 // Define custom ArrayAdapter for displaying items in a ListView
 class ItemAdapter(
@@ -73,7 +77,6 @@ class ItemAdapter(
         // Return the created view for this item
         return view
     }
-
 }
 
 class MainActivity : AppCompatActivity() {
@@ -82,10 +85,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var data: NasaResponse
 
-    private var currentPage = 1
     private var currentQuery = ""
-    //only 15 results will show per page with a max of 100 pages
-    private val resultsPerPage = 15
+    private var displayedPage = 1
     private val maxPages = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -93,41 +94,46 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Fetch data with an initial query
+        // Fetch data with an initial query and page number.
         fetchData("a")
 
-        // Set onClickListeners for prevButton and nextButton to navigate between pages of results
         binding.prevButton.setOnClickListener {
-            if (currentPage > 1) {
-                currentPage -= 1
-                updatePage()
+            if (displayedPage > 1) {
+                // disabled until data from API is retrieved
+                binding.prevButton.isEnabled = false
+                binding.nextButton.isEnabled = false
+                displayedPage -= 1
+                fetchData(currentQuery)
             }
         }
 
         binding.nextButton.setOnClickListener {
-            if (currentPage < maxPages) {
-                currentPage += 1
-                updatePage()
+            if (displayedPage < maxPages) {
+                binding.prevButton.isEnabled = false
+                binding.nextButton.isEnabled = false
+                displayedPage += 1
+                fetchData(currentQuery)
             }
         }
     }
 
-    // Fetch data from NASA API using OkHttp library and update UI with results on success or print error message on failure.
     private fun fetchData(query: String) {
         try {
             currentQuery = query
 
-            // Build URL for NASA API request using query parameter.
-            val url = "https://images-api.nasa.gov/search?q=$query"
+            // Build URL for NASA API request using query and displayedPage parameters.
+            val url = "https://images-api.nasa.gov/search?q=$query&page=$displayedPage"
             val request = Request.Builder().url(url).build()
             val client = OkHttpClient()
             client.newCall(request).enqueue(object : Callback {
-
                 override fun onResponse(call: Call, response: Response) {
                     try {
                         val body = response.body?.string()
                         val json = Json { ignoreUnknownKeys = true }
                         data = json.decodeFromString(body!!)
+                    } catch (e: Exception) {
+                        println("Error: " + e.message)
+                    } finally {
                         runOnUiThread {
                             // Update the data used by the ArrayAdapter on the UI thread after fetching data from API.
                             updatePage()
@@ -135,11 +141,8 @@ class MainActivity : AppCompatActivity() {
                             // Set up SearchView to allow user to search for new query or filter current results by title.
                             setupSearchBar()
                         }
-                    } catch (e: Exception) {
-                        println("Error: " + e.message)
                     }
                 }
-
                 override fun onFailure(call: Call, e: IOException) {
                     println("API execute failed")
                 }
@@ -149,91 +152,69 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-    // Update UI with current page of results and enable/disable prevButton and nextButton as needed.
     private fun updatePage() {
-        val start = (currentPage - 1) * resultsPerPage
-        // This makes sure that the end index does not pass the last item in the list of items returned by the NASA API.
-        val end = min(start + resultsPerPage, data.collection.items.size)
-        if (start <= end) {
-            // Create a new list with the updated data for current page of results.
-            val newItems = data.collection.items.subList(start, end)
-            // Pass the new list to a new instance of the ItemAdapter and set it as adapter for userList ListView.
-            val adapter = ItemAdapter(this, newItems)
-            binding.userList.adapter = adapter
+        // Pass all items returned by the NASA API to a new instance of the ItemAdapter and set it as adapter for userList ListView.
+        val adapter = ItemAdapter(this, data.collection.items)
+        binding.userList.adapter = adapter
 
-            // Set text for pageText TextView to display current page number.
-            binding.pageText.text = "Page $currentPage"
+        // Set text for pageText TextView to display current page number.
+        binding.pageText.text = "Page $displayedPage"
 
-            // Set onClickListener for userList items to start ItemPageActivity when an item is clicked.
-            binding.userList.setOnItemClickListener { parent, view, position, id ->
-                val selectedItem = parent.getItemAtPosition(position) as Item
+        // Set onClickListener for userList items to start ItemPageActivity when an item is clicked.
+        binding.userList.setOnItemClickListener { parent, view, position, id ->
+            val selectedItem = parent.getItemAtPosition(position) as Item
 
-                // Create Intent to start ItemPageActivity.
-                val intent = Intent(this, ItemPageActivity::class.java)
+            // Create Intent to start ItemPageActivity.
+            val intent = Intent(this, ItemPageActivity::class.java)
 
-                // Pass data from the selected item to the new Activity using intent extras.
-                intent.putExtra("title", selectedItem.data[0].title)
-                intent.putExtra("description", selectedItem.data[0].description)
-                intent.putExtra("date_created", selectedItem.data[0].date_created)
-                intent.putExtra("nasa_id", selectedItem.data[0].nasa_id)
+            // Pass data from the selected item to the new Activity using intent extras.
+            intent.putExtra("title", selectedItem.data[0].title)
+            intent.putExtra("description", selectedItem.data[0].description)
+            intent.putExtra("date_created", selectedItem.data[0].date_created)
+            intent.putExtra("nasa_id", selectedItem.data[0].nasa_id)
 
-                selectedItem.links?.firstOrNull()?.href?.let { url ->
-                    intent.putExtra("imageUrl", url)
-                }
-
-                startActivity(intent)
+            selectedItem.links?.firstOrNull()?.href?.let { url ->
+                intent.putExtra("imageUrl", url)
             }
+            startActivity(intent)
         }
 
         // Enable/disable prevButton and nextButton based on current page number and availability of more results from API.
-        binding.prevButton.isEnabled = currentPage > 1
-        binding.nextButton.isEnabled = currentPage < maxPages && end < data.collection.items.size
+        binding.prevButton.isEnabled = displayedPage > 1
+        binding.nextButton.isEnabled =
+            displayedPage < maxPages
     }
 
-    // Filter current page of results by title using user's query and update UI with filtered results.
     private fun filterItems(query: String) {
-        val start = (currentPage - 1) * resultsPerPage
-        val end = min(start + resultsPerPage, data.collection.items.size)
         val filteredItems =
-            data.collection.items.subList(start, end).filter { item -> item.data[0].title.contains(query, ignoreCase = true) }
+            data.collection.items.filter { item -> item.data[0].title.contains(query, ignoreCase = true) }
         val adapter = ItemAdapter(this@MainActivity, filteredItems)
         binding.userList.adapter = adapter
 
-        // Disable nextButton if there are no more filtered results available on current page.
         binding.nextButton.isEnabled =
-            currentPage < maxPages && end < data.collection.items.size && filteredItems.size >= resultsPerPage
-
+            displayedPage < maxPages && filteredItems.size >= 100
     }
+
 
     // Set up SearchView to allow user to search for new query or filter current results by title.
     private fun setupSearchBar() {
-
         binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 binding.searchView.clearFocus()
-
-                // Fetch data with the user's query and reset the page number when user submits a new query.
-                currentPage = 1
-                fetchData(query ?: "")
-                updatePage()
-
+                updateData(query ?: "")
                 return false
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                // Fetch data with the user's query and reset the page number when user changes a new query.
-                currentPage = 1
-
-                // Fetch data with the user's query when user types any characters in SearchView.
-                fetchData(newText ?: "")
-
-                // Filter items and update adapter after data has been fetched.
-                filterItems(newText ?: "")
+                updateData(newText ?: "")
                 return false
             }
-
         })
     }
-}
 
+    private fun updateData(query: String) {
+        displayedPage = 1
+        fetchData(query)
+        updatePage()
+    }
+}
